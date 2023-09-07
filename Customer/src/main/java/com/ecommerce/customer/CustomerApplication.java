b92ece013eb56343be08e98881841af478240297
an bình <BinhAn1676>
2023-09-08 02:11:15 +0700

    start building customer html

--- Customer/src/main/java/com/ecommerce/customer/CustomerApplication.java
+++ Customer/src/main/java/com/ecommerce/customer/CustomerApplication.java
@@ -3,11 +3,14 @@ package com.ecommerce.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.ecommerce.customer")
+@SpringBootApplication(scanBasePackages = "com.ecommerce.customer", exclude = { SecurityAutoConfiguration.class }  )
 @EnableJpaRepositories(value = "com.ecommerce.library.repository")
 @EntityScan(value = "com.ecommerce.library.model")
+@ComponentScan(value = {"com.ecommerce.library.repository","com.ecommerce.customer"})
 public class CustomerApplication {
 
 	public static void main(String[] args) {
